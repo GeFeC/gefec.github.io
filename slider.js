cda76a850ce63a87e97b6c8e974c78389b9af619
gefec <gefec@debian.wojtek-debian.com>
2026-08-27 17:35:53 +0200

    Update: Added input:number to sliders

--- slider.js
+++ slider.js
@@ -6,6 +6,10 @@ window.Slider = function Slider(props){
   const { on_change } = props;
 
   const handle_on_change = e => {
+    if (e.target.value == ""){
+      return;
+    }
+
     set_val(e.target.value);
     on_change(parseFloat(e.target.value));
   }
@@ -13,6 +17,24 @@ window.Slider = function Slider(props){
   const min = props.min || 0;
   const max = props.max || 1;
   const step = props.step || 0.05;
+
+  const handle_on_number_change = e => {
+    if (e.target.value == "") {
+      set_val("");
+    }
+
+    let new_val = parseFloat(e.target.value);
+
+    if (!isNaN(new_val)){
+      new_val = clamp(new_val, min, max);
+      set_val(new_val);
+      on_change(new_val);
+    }
+    else{
+      set_val(e.target.value)
+    }
+
+  };
 
   const style = {
     minWidth: '350px',
@@ -27,9 +49,25 @@ window.Slider = function Slider(props){
     alignItems: 'center'
   };
 
+  const number_input_style = {
+    width: '50px',
+    border: 'none',
+    outline: 'none'
+  }
+
   return (
     <div style={props.style || style}>
-      <span>{name} : {val}</span>
+      <span>
+        {name} 
+        : 
+        <input 
+          type="number" 
+          value={val}
+          style={number_input_style}
+          onChange={ handle_on_number_change }
+          step={ step }
+        />
+      </span>
 
       <div style={slider_wrapper_style}>
         { min }
